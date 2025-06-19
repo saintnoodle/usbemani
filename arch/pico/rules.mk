9f1e3872c9b0c4a427e6cb66b34a2ab123f9bc14
aixxe <me@aixxe.net>
2025-06-19 14:15:31 +0100

    Support defining custom Pico board & platforms

--- arch/pico/rules.mk
+++ arch/pico/rules.mk
@@ -2,6 +2,10 @@ BUILD_SYSTEM = CMake
 ARCH         = pico
 ARCH_PATH    = $(ROOT_DIR)/arch/pico
 
+PICO_BOARD             ?= pico
+PICO_PLATFORM          ?= rp2040
+PICO_BOARD_HEADER_DIRS ?= $(ROOT_DIR)/boards/$(TARGET_BOARD)
+
 SRC += $(wildcard $(ARCH_PATH)/*.c)
 SRC += $(wildcard $(ARCH_PATH)/impl/*.c)
 
@@ -11,7 +15,7 @@ INCLUDES += $(ARCH_PATH)/
 .PHONY: all clean cmake_prep
 
 cmake_prep:
-	@mkdir -p $(OBJDIR)/.generated && cd $(OBJDIR) && cmake -DTARGET_NAME=${CLEAN_NAME} -DROOT_DIR=$(ROOT_DIR) $(ROOT_DIR)/arch/pico/ -DOBJ_DIR=$(OBJDIR) -DINCLUDES="$(INCLUDES)" -DSRC="$(SRC)"
+	@mkdir -p $(OBJDIR)/.generated && cd $(OBJDIR) && cmake -DTARGET_NAME=${CLEAN_NAME} -DPICO_BOARD_HEADER_DIRS=$(PICO_BOARD_HEADER_DIRS) -DPICO_BOARD=$(PICO_BOARD) -DPICO_PLATFORM=$(PICO_PLATFORM) -DROOT_DIR=$(ROOT_DIR) $(ROOT_DIR)/arch/pico/ -DOBJ_DIR=$(OBJDIR) -DINCLUDES="$(INCLUDES)" -DSRC="$(SRC)"
 
 all: cmake_prep
 	@cd $(OBJDIR) && make
